e54f6c701b0b1876234b401484a13fd92ffc3ae4
Kremo <62044643+Kremo28@users.noreply.github.com>
2020-09-28 18:32:13 -0500

    Update example.js

--- example.js
+++ example.js
@@ -9,3 +9,15 @@ local.get("nombre"); //llamar un objeto
 local.remove("nombre"); //borrar un objeto
 
 local.clear("nombre"); //eliminar valores un objeto
+
+/* O */
+
+local().new("nombre", "valor"); //crear un objeto
+
+local().get("nombre"); //llamar un objeto
+
+local().remove("nombre"); //borrar un objeto
+
+local().clear("nombre"); //eliminar valores un objeto
+
+local().change("nombre", "nuevo valor"); //cambiar el valor de un objeto
