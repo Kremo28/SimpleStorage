ab0fc38e85e7b06a09905d3f01eee08b719783db
Kremo <62044643+Kremo28@users.noreply.github.com>
2020-09-29 14:11:11 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,3 +1,18 @@
+const local = require("./smplestorage.module.js")
+
+local().new("nombre", "valor"); //crear un objeto
+
+local().get("nombre"); //llamar un objeto
+
+local().remove("nombre"); //borrar un objeto
+
+local().clear("nombre"); //eliminar valores un objeto
+
+local().change("nombre", "nuevo valor"); //cambiar el valor de un objeto
+
+
+//No Module
+
 local().new("nombre", "valor"); //crear un objeto
 
 local().get("nombre"); //llamar un objeto
