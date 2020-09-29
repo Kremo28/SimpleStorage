5d38cfd1d72c542a10f81a348c4528489c30fc4f
Kremo <62044643+Kremo28@users.noreply.github.com>
2020-09-28 19:58:13 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,19 +1,3 @@
-/* Modules */
-
-const local = require('./simplestorage.min.js');
-
-local.new("nombre", "valor"); //crear un objeto
-
-local.change("nombre", "nuevo valor"); //cambiar el valor de un objeto
-
-local.get("nombre"); //llamar un objeto
-
-local.remove("nombre"); //borrar un objeto
-
-local.clear("nombre"); //eliminar valores un objeto
-
-/* No Modules */
-
 local().new("nombre", "valor"); //crear un objeto
 
 local().get("nombre"); //llamar un objeto
